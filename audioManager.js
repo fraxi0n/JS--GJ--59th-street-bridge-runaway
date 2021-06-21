b9913c78b28ae03284c3ee6b07a756933d5b1735
Yoann Claude <58945864+YoannClaude@users.noreply.github.com>
2021-06-21 11:43:43 +0200

    fixing path issue for itch.io

--- audioManager.js
+++ audioManager.js
@@ -74,7 +74,7 @@ function CHECK_RESULT(result)
 // Call FMOD file preloading functions here to mount local files.  Otherwise load custom data from memory or use own file system. 
 function prerun() 
 {
-    var fileUrl = "/audio/Desktop/";
+    var fileUrl = "./banks/";
     var fileName;
     var folderName = "/";
     var canRead = true;
